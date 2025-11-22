7c69c9608a75241f9fda01ea6065ac8e81d73550
Dinesh Kumar <dineshdk672000@gmail.com>
2025-11-22 12:41:12 +0530

    Data Wrangler Code complete

--- Data_wrangler_pipeline/Cleaning_pipeline.py
+++ Data_wrangler_pipeline/Cleaning_pipeline.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
+st.title("Data Wrangler Pipeline")
+st.write("This dashboard shows the statistics of a ecommerce customer database before cleaning and after cleaning using the data wrangler pipeline")
+
 
 def fill_null_with_rand_values(df: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -26,12 +29,61 @@ def fill_null_with_rand_values(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def display_stats(df: pd.DataFrame) -> None:
+
+    # Displaying null values
+    nulls = df.isnull().sum()
+    plt.bar(nulls.index.to_list(), nulls.values)
+    plt.title("Distribution of Null values across Columns")
+    plt.tick_params(axis='x', labelrotation=90)
+    plt.xlabel("Columns")
+    plt.ylabel("Null Values")
+    plt.tight_layout()
+    st.pyplot(plt)
+
+    ignore_cols = ["CustomerID", "RegistrationDate",
+                   'FavoriteCategory', 'SecondFavoriteCategory']
+    for col in ignore_cols:
+        if col in df.columns:
+            df = df.drop(columns=[col])
+
+    # Displaying Column Distribution in Streamlit
+    # Categorical columns
+    cat_cols = df.select_dtypes(include='object').columns
+    cat_cols = cat_cols[1:]
+    cat_rows = int(np.ceil(len(cat_cols)/2))
+    fig, ax = plt.subplots(4, 2, figsize=(cat_rows*3, 8), layout="constrained")
+    ax = ax.flatten()
+
+    for i, col in enumerate(cat_cols):
+        count = df[col].value_counts()
+        ax[i].bar(count.index.to_list(), count)
+        ax[i].set_title(col)
+        ax[i].tick_params(axis='x', labelrotation=90)
+    plt.suptitle("Categorical Columns Distribution")
+    st.pyplot(plt)
+
+    # Numeric columns
+    num_cols = df.select_dtypes(include=['int', 'float']).columns
+    num_rows = int(np.ceil(len(num_cols)/3))
+    fig, axes = plt.subplots(num_rows, 3, layout='constrained',
+                             figsize=(12, num_rows*3))
+    axes = axes.flatten()
+    plt.suptitle("Numerical Columns Distribution")
+    for i, col in enumerate(num_cols):
+        axes[i].hist(df[col])
+        axes[i].set_title(col)
+    st.pyplot(plt)
+    fig.clear()
+
+
 df = pd.read_csv("ecommerce_customer_data.csv")
 print(df.info())
 
-df = df.drop(columns=['FavoriteCategory', 'SecondFavoriteCategory'])
+st.header("Data statistics before cleaning:")
+display_stats(df)
 
-# print(df.isna().sum())
+df = df.drop(columns=['FavoriteCategory', 'SecondFavoriteCategory'])
 
 # Cleaning CustomerID by trimming the values and dropping rows with nulls
 df["CustomerID"] = df["CustomerID"].str[4:]
@@ -46,20 +98,11 @@ df["RegistrationDate"] = pd.to_datetime(df["RegistrationDate"])
 
 # Imputing improper values in Age with random values of its own distribution
 df["Age"] = df["Age"].astype(int)
-
-fig, axes = plt.subplots(1, 2)
-axes[0].hist(df["Age"])
-
 data = df["Age"][df["Age"] >= 18]
 size = df["Age"][df["Age"] < 18].count()
 df.loc[df["Age"] < 18, "Age"] = np.random.choice(data, size=size, replace=True)
 
-axes[1].hist(df["Age"])
-plt.show()
 
-for col in df.select_dtypes(include='object'):
-    print(col, ":")
-    print(df[col].unique())
 # Standardizing categories in columns
 df.loc[df["Gender"] == 'Male', "Gender"] = 'M'
 df.loc[df["Gender"] == 'Female', "Gender"] = 'F'
@@ -67,14 +110,5 @@ df.loc[df["Gender"] == 'Female', "Gender"] = 'F'
 df.loc[df["IncomeLevel"] == 'L', "IncomeLevel"] = 'Low'
 df.loc[df["IncomeLevel"] == 'H', "IncomeLevel"] = 'High'
 
-num_cols = df.select_dtypes(include=['int', 'float']).columns
-num_rows = int(np.ceil(len(num_cols)/4))
-fig, axes = plt.subplots(num_rows, 4, layout='constrained',
-                         figsize=(len(num_cols)*4, num_rows*3))
-axes = axes.flatten()
-
-for i, col in enumerate(num_cols):
-    axes[i].hist(df[col])
-    axes[i].set_title(col)
-
-plt.show()
+st.header("Data statistics after cleaning:")
+display_stats(df)
